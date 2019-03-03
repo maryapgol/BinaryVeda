146824738a68aaa01a71cb3390ae65c8508b7c46
Aniket <amaryapgol3@gmail.com>
2019-03-03 19:01:25 +0530

    Added current working, designation

--- app/src/main/java/com/binaryveda/aniket/binaryveda/View/MainActivity.kt
+++ app/src/main/java/com/binaryveda/aniket/binaryveda/View/MainActivity.kt
@@ -60,15 +60,26 @@ class MainActivity : AppCompatActivity(),BaseContract.View {
         small_paramLin.setMargins(18*density.toInt(),45*density.toInt(),0,0)
         small_paramLin.addRule(RelativeLayout.RIGHT_OF, ivSeeker.id)
 
+
         linSeekerData.layoutParams=small_paramLin
         ivSeeker.layoutParams=small_param
         Glide.with(applicationContext).load(data!!.image).asBitmap().crossFade(800).into(ivSeeker)
 
+        linDetail.layoutParams.height=80*density.toInt()
+        linDetail.requestLayout()
         txtName.text=data.name
         txtdesignation.text=data.designation!!.name
         txtLocation.text=data.location
 
+//Add qualification, ctc, experience
 
+
+        txtQualification.text=data.highest_qualification!!.name
+        txtExperiance.text=data.experience
+        txtExpectedctc.text=data.expected_ctc
+
+        txtCurrentWorking.text=data.last_company!!.name
+        txtDesignation.text=data.designation.name
     }
 
 
